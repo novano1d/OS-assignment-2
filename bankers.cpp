4251a53942c7af300d5e516c839ae8bdaa6612bf
novano1d <mmoor179@kent.edu>
2023-04-28 18:55:42 -0400

    Update bankers.cpp

--- bankers.cpp
+++ bankers.cpp
@@ -4,39 +4,6 @@
 #include <string>
 const int NUM_PROCESSES = 5;
 const int NUM_RESOURCES = 3;
-bool checkSafe(int processesArr[], int available[], int max[][NUM_RESOURCES], int allocated[][NUM_RESOURCES]);
-int main()
-{
-    int processesArr[] = { 0, 1, 2, 3, 4 };
-    std::ifstream table("input.txt", std::ios_base::in);
-    if (!table) 
-    {
-        std::cout << "Failed to open input.txt." << std::endl;
-        return EXIT_FAILURE;
-    }
-    int max[NUM_PROCESSES][NUM_RESOURCES];
-    int allocated[NUM_PROCESSES][NUM_RESOURCES];
-    int available[NUM_RESOURCES];
-    for (int i = 0; i < NUM_PROCESSES; ++i) 
-    { 
-        for (int j = 0; j < NUM_RESOURCES; ++j) table >> allocated[i][j];
-        for (int j = 0; j < NUM_RESOURCES; ++j) table >> max[i][j];
-        if (i == 0) for (int j = 0; j < NUM_RESOURCES; ++j) table >> available[j];
-    }
-    std::cout << "Allocated: ";
-    for (int i = 0; i < NUM_PROCESSES; ++i)
-        for (int j = 0; j < NUM_RESOURCES; ++j) std::cout << allocated[i][j] << ' ';
-    std::cout << std::endl;
-    std::cout << "Max: ";
-    for (int i = 0; i < NUM_PROCESSES; ++i)
-        for (int j = 0; j < NUM_RESOURCES; ++j) std::cout << max[i][j] << ' ';
-    std::cout << std::endl;
-    std::cout << "Available: ";
-    for (int i = 0; i < NUM_RESOURCES; ++i) std::cout << available[i] << ' ';
-    std::cout << std::endl;
-    checkSafe(processesArr, available, max, allocated);
-    return EXIT_SUCCESS;
-}
 bool checkSafe(int processesArr[], int available[], int max[][NUM_RESOURCES], int allocated[][NUM_RESOURCES]) 
 {
     int need[NUM_PROCESSES][NUM_RESOURCES];
@@ -80,4 +47,36 @@ bool checkSafe(int processesArr[], int available[], int max[][NUM_RESOURCES], in
     for (int i = 0; i < NUM_PROCESSES ; i++) std::cout << sequence[i] << ' ';
     std::cout << std::endl;
     return true;
+}
+int main()
+{
+    int processesArr[] = { 0, 1, 2, 3, 4 };
+    std::ifstream table("input.txt", std::ios_base::in);
+    if (!table) 
+    {
+        std::cout << "Failed to open input.txt." << std::endl;
+        return EXIT_FAILURE;
+    }
+    int max[NUM_PROCESSES][NUM_RESOURCES];
+    int allocated[NUM_PROCESSES][NUM_RESOURCES];
+    int available[NUM_RESOURCES];
+    for (int i = 0; i < NUM_PROCESSES; ++i) 
+    { 
+        for (int j = 0; j < NUM_RESOURCES; ++j) table >> allocated[i][j];
+        for (int j = 0; j < NUM_RESOURCES; ++j) table >> max[i][j];
+        if (i == 0) for (int j = 0; j < NUM_RESOURCES; ++j) table >> available[j];
+    }
+    std::cout << "Allocated: ";
+    for (int i = 0; i < NUM_PROCESSES; ++i)
+        for (int j = 0; j < NUM_RESOURCES; ++j) std::cout << allocated[i][j] << ' ';
+    std::cout << std::endl;
+    std::cout << "Max: ";
+    for (int i = 0; i < NUM_PROCESSES; ++i)
+        for (int j = 0; j < NUM_RESOURCES; ++j) std::cout << max[i][j] << ' ';
+    std::cout << std::endl;
+    std::cout << "Available: ";
+    for (int i = 0; i < NUM_RESOURCES; ++i) std::cout << available[i] << ' ';
+    std::cout << std::endl;
+    checkSafe(processesArr, available, max, allocated);
+    return EXIT_SUCCESS;
 }
